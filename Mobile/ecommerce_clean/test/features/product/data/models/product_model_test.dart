5ded5c042b3a15aac17f322c97bc14587ab9b5b1
ikramawol <ikruawol.14@gmail.com>
2024-08-08 10:35:45 +0300

    done with the models

--- Mobile/ecommerce_clean/test/features/product/data/models/product_model_test.dart
+++ Mobile/ecommerce_clean/test/features/product/data/models/product_model_test.dart
@@ -5,65 +5,41 @@ import 'package:ecommerce_clean/features/product/domain/entities/product.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../../helper/json_reader.dart';
-// import 'package:json_reader.dart';
 
-void main(){
-
-  // const testProductModel = ProductModel(
-  //   id: '1',
-  //   name: 'Nike Air Max',
-  //   catagory: 'Men\'s shoes',
-  //   description: 'its a shoe',
-  //   price: 100,
-  //   imageUrl: 'image',
-  // );
+void main() {
   const testProductModel = ProductModel(
     id: '6672752cbd218790438efdb0',
     name: 'Anime website',
     catagory: 'Anime',
     description: 'Explore anime characters.',
     price: 123,
-    imageUrl: 'https://res.cloudinary.com/g5-mobile-track/image/upload/v1718777132/images/zxjhzrflkvsjutgbmr0f.jpg',
+    imageUrl:
+        'https://res.cloudinary.com/g5-mobile-track/image/upload/v1718777132/images/zxjhzrflkvsjutgbmr0f.jpg',
   );
-  test(
-    'should be subclass of Product Entity',
-    () async {
-      // assert
-      expect(testProductModel, isA<ProductEntity>());
-    }
-  );
-  test(
-    'should return a valid model from json',
-    () async {
+  test('should be subclass of Product Entity', () async {
+    // assert
+    expect(testProductModel, isA<ProductEntity>());
+  });
 
+  /// checks whether the ProductModel can be correctly created from JSON data
+  test('should return a valid model from json', () async {
+    // arrange
     final Map<String, dynamic> jsonMap = json.decode(
-        readJson('helper/dummy_data/dummy_product_response.json'),
-        )['data'][0];
+      readJson('helper/dummy_data/dummy_product_response.json'),
+    )['data'][0];
     // act
 
     final result = ProductModel.fromJson(jsonMap);
-    
 
     // assert
     expect(result, equals(testProductModel));
-  }
-  );
+  });
 
-  test(
-    'should return a json map containing the proper data',
-    () async {
-      //act
-      final result = testProductModel.toJson();
-      //assert
-      final expectedMap = {
-        "id": "6672752cbd218790438efdb0",
-        "name": "Anime website",
-        "catagory": "Anime",
-        "description": "Explore anime characters.",
-        "price": 123,
-        "imageUrl": "https://res.cloudinary.com/g5-mobile-track/image/upload/v1718777132/images/zxjhzrflkvsjutgbmr0f.jpg"
-      };
-      expect(result, expectedMap);
-    }
-  );
+  /// verifies that your ProductModel can correctly serialize(arrange) its data to a JSON map. It converts the testProductModel instance to a JSON map and checks if it matches the expected JSON structure
+  test('should return a json map containing the proper data', () async {
+    //act
+    final result = testProductModel.toJson();
+    //assert
+    expect(result, equals(testProductModel.toJson()));
+  });
 }
